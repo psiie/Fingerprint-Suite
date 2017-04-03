5e46d9dff5d1ff67a9934fabd00cab73467aaeb1
darkenvy <darkenvy6@gmail.com>
2017-04-03 14:55:51 -0700

    click events

--- js/popup.js
+++ js/popup.js
@@ -1,2 +1,17 @@
-console.log('popupjs ', blacklist);
 
+document.addEventListener('DOMContentLoaded', function() {
+  // console.log('popupjs ', blacklist);
+  // load settings from background
+
+
+  var pauseBtn = document.getElementById('pause');
+  var blacklistBtn = document.getElementById('blacklist');
+  
+  pauseBtn.addEventListener('click', function() {
+    pauseBtn.innerText = 'clicked';
+  });
+  blacklistBtn.addEventListener('click', function() {
+    blacklistBtn.innerText = 'clicked';
+  });
+
+});
